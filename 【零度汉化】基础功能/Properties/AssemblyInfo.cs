9a72205cca381326342cfad927a7db6c0fc18482
零度汉化 <81199000@qq.com>
2015-08-22 19:44:46 +0800

    汉化

--- 【零度汉化】基础功能/Properties/AssemblyInfo.cs
+++ 【零度汉化】基础功能/Properties/AssemblyInfo.cs
@@ -60,6 +60,6 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.454")]
-[assembly: AssemblyFileVersion("1.0.0.454")]
+[assembly: AssemblyVersion("1.0.0.455")]
+[assembly: AssemblyFileVersion("1.0.0.455")]
 [assembly: NeutralResourcesLanguage("en")]
