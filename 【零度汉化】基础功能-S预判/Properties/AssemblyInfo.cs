f5ea05b25768d05eb35484c36b6b6522c8b7f703
零度汉化 <81199000@qq.com>
2015-09-15 16:31:48 +0800

    汉化

--- 【零度汉化】基础功能-S预判/Properties/AssemblyInfo.cs
+++ 【零度汉化】基础功能-S预判/Properties/AssemblyInfo.cs
@@ -60,6 +60,6 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.45")]
-[assembly: AssemblyFileVersion("1.0.0.45")]
+[assembly: AssemblyVersion("1.0.0.451")]
+[assembly: AssemblyFileVersion("1.0.0.451")]
 [assembly: NeutralResourcesLanguage("en")]
